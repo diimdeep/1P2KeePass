d5372e5ec09f796c1c198db30686ae2ae1d518ab
Dmitry Wolf <dmitry.s.wolf@gmail.com>
2017-05-16 09:43:07 +0300

    Fix timestamp represented as millis #2

--- 1P2KeePass/Common/DateTimeExt.cs
+++ 1P2KeePass/Common/DateTimeExt.cs
@@ -8,7 +8,12 @@ namespace _1Password2KeePass
 		{
 			// Unix timestamp is seconds past epoch
 			System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-			dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+
+            // fix bug when timestamp is millis instead of seconds [issues/2]
+            if (unixTimeStamp > 20000000000)			
+				dtDateTime = dtDateTime.AddMilliseconds(unixTimeStamp).ToLocalTime();			
+			else
+				dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
 			return dtDateTime;
 		}
 	}
